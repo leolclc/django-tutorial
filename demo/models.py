acc9bba7e6aa8d5e9448053a275bb108bc0359b8
leolclc <leandrocesar2006@gmail.com>
2022-03-16 23:02:36 -0300

    Fields

--- demo/models.py
+++ demo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
 
 class Book(models.Model):
-    title = models.CharField(max_length=36)
+
+    title = models.CharField(max_length=36,unique=True, blank=False)
