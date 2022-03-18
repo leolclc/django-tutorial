f753c7be887e32cf3644ff6454a2a4d9d80a4e45
leolclc <leandrocesar2006@gmail.com>
2022-03-17 23:05:09 -0300

    class views

--- demo/models.py
+++ demo/models.py
@@ -4,4 +4,11 @@ from django.db import models
 
 class Book(models.Model):
 
-    title = models.CharField(max_length=36,unique=True, blank=False)
+    title = models.CharField(max_length=36, unique=True, blank=False)
+    description = models.TextField(max_length=256, blank=True)
+
+    price = models.DecimalField(default=0, max_digits=4, decimal_places=2)
+
+    published = models.DateField(blank=True, null=True, default=None)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='covers/', blank=True)
